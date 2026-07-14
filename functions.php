c831d43d7241932ff3ec9d7305cf091e4eb33e68
Rambir <r9896633156@gmail.com>
2026-07-14 12:10:04 +0530

    feat: Update theme version to 1.0.1 and add hero button styles for improved UI

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define Theme Constants.
-define( 'ROBO_THEME_VERSION', '1.0.0' );
+define( 'ROBO_THEME_VERSION', '1.0.1' );
 define( 'ROBO_THEME_DIR', get_template_directory() );
 define( 'ROBO_THEME_URI', get_template_directory_uri() );
 
